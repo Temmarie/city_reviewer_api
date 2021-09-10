e8db2b4ddb6304494b3b5928b694a6a29a038415
Temmarie <temmar_whyte16@yahoo.com>
2021-09-10 14:56:24 +0100

    remove port

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-root 'cities#index'
+
 
   namespace :api do
     namespace :v1 do
-      resources :cities, only: [:create, :destroy, :show, :index], param: :slug
+      resources :cities, param: :slug
       resources :reviews, only: [:create, :destroy, :show, :index]
     end
   end
